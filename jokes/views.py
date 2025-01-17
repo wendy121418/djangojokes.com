c4ae385ec4076e4b00d6e2393df1e0778e0c7d35
Wennie P. Catabay <wenniecatabay@gmail.com>
2025-01-17 15:08:14 +0800

    CRUDL (Create, Read, Update, Delete, List)

---
+++ jokes/views.py
@@ -0,0 +1,24 @@
+from django.urls import reverse_lazy
+
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+
+from .models import Joke
+
+class JokeCreateView(CreateView):
+        model = Joke
+        fields = ['question', 'answer']
+
+class JokeDeleteView(DeleteView):
+        model = Joke
+        success_url = reverse_lazy('jokes:list')
+
+class JokeDetailView(DetailView):
+        model = Joke
+
+class JokeListView(ListView):
+        model = Joke
+        
+
+class JokeUpdateView(UpdateView):
+        model = Joke
+        fields = ['question', 'answer']
